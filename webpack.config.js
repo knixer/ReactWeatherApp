c3c5569ed0d1efb005161e6257640796ffafdad4
knixer <christopher.ono@gmail.com>
2019-05-14 16:17:57 +0200

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
+module.exports = {
+    mode: 'development',
+    entry: {
+      app: SRC_DIR + '/app/index.js'
+    },
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "env",
+                            "react"
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+}
